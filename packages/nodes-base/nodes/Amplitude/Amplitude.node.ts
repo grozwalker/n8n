66bf5a70236413846f45707787daf77bb4edc816
Andrey Grozs <grozwalker@gmail.com>
2022-04-20 11:43:44 +0000

    add amplitude

--- packages/nodes-base/nodes/Amplitude/Amplitude.node.ts
+++ packages/nodes-base/nodes/Amplitude/Amplitude.node.ts
@@ -7,6 +7,7 @@ import {
   INodeExecutionData,
   INodeType,
   INodeTypeDescription,
+  LoggerProxy as Logger,
 } from 'n8n-workflow';
 
 import {
@@ -16,7 +17,7 @@ import {
 export class Amplitude implements INodeType {
 
   description: INodeTypeDescription = {
-      displayName: 'Amplitude 1',
+      displayName: 'Amplitude',
       name: 'amplitude',
       icon: 'file:amplitude.svg',
       group: ['transform'],
@@ -41,52 +42,148 @@ export class Amplitude implements INodeType {
           type: 'options',
           options: [
               {
-                  name: 'Contact',
-                  value: 'contact',
+                  name: 'Event',
+                  value: 'event',
               },
           ],
-          default: 'contact',
+          default: 'event',
           required: true,
           description: 'Resource to consume',
         },
-        // {
-        //     displayName: 'Operation',
-        //     name: 'operation',
-        //     type: 'options',
-        //     displayOptions: {
-        //         show: {
-        //             resource: [
-        //                 'contact',
-        //             ],
-        //         },
-        //     },
-        //     options: [
-        //         {
-        //             name: 'Create',
-        //             value: 'create',
-        //             description: 'Create a contact',
-        //         },
-        //     ],
-        //     default: 'create',
-        //     description: 'The operation to perform.',
-        // },
         {
-            displayName: 'Email',
-            name: 'email',
+            displayName: 'Operation',
+            name: 'operation',
+            type: 'options',
+            displayOptions: {
+                show: {
+                    resource: [
+                        'event',
+                    ],
+                },
+            },
+            options: [
+                {
+                    name: 'Send',
+                    value: 'send',
+                    description: 'Send an event',
+                },
+            ],
+            default: 'send',
+            description: 'The operation to perform.',
+        },
+        {
+            displayName: 'Event Type',
+            name: 'event_type',
             type: 'string',
             required: true,
             displayOptions: {
                 show: {
                     operation: [
-                        'create',
+                        'send',
                     ],
                     resource: [
-                        'contact',
+                        'event',
+                    ],
+                },
+            },
+            default:'test_ag',
+            description:'Event name',
+        },
+        {
+            displayName: 'User ID',
+            name: 'user_id',
+            type: 'string',
+            required: true,
+            displayOptions: {
+                show: {
+                    operation: [
+                        'send',
+                    ],
+                    resource: [
+                        'event',
+                    ],
+                },
+            },
+            default:'12345678',
+            description:'User ID',
+        },
+        {
+            displayName: 'Event Timestamp',
+            name: 'event_timestamp',
+            type: 'string',
+            required: true,
+            displayOptions: {
+                show: {
+                    operation: [
+                        'send',
+                    ],
+                    resource: [
+                        'event',
+                    ],
+                },
+            },
+            default:'345',
+            description:'Event Timestamp',
+        },
+        {
+            displayName: 'Insert ID prefix',
+            name: 'insert_id_prefix',
+            type: 'string',
+            required: false,
+            displayOptions: {
+                show: {
+                    operation: [
+                        'send',
+                    ],
+                    resource: [
+                        'event',
                     ],
                 },
             },
             default:'',
-            description:'Primary email for the contact',
+            description:'Insert ID prefix',
+        },
+        {
+            displayName: 'Event Properties',
+            name: 'event_properties',
+            type: 'json',
+            typeOptions: {
+              alwaysOpenEditWindow: true,
+            },
+            required: false,
+            displayOptions: {
+                show: {
+                    operation: [
+                        'send',
+                    ],
+                    resource: [
+                        'event',
+                    ],
+                },
+            },
+            default:'',
+            description:'Event Properties',
+        },
+        {
+            displayName: 'User Properties',
+            name: 'user_properties',
+            type: 'json',
+            typeOptions: {
+              alwaysOpenEditWindow: true,
+            },
+            required: false,
+            displayOptions: {
+                show: {
+                    operation: [
+                        'send',
+                    ],
+                    resource: [
+                        'event',
+                    ],
+                },
+            },
+            default:'',
+            description:'User Properties',
         },
 
           // Node properties which the user gets displayed and
@@ -94,35 +191,39 @@ export class Amplitude implements INodeType {
       ],
   };
 
-
   async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
-    console.log('run')
     let responseData;
     const resource = this.getNodeParameter('resource', 0) as string;
     const operation = this.getNodeParameter('operation', 0) as string;
     //Get credentials the user provided for this node
-    // const credentials = await this.getCredentials('amplitudeApi') as IDataObject;
+    const credentials = await this.getCredentials('amplitudeApi') as IDataObject;
 
     if (resource === 'event') {
         if (operation === 'send') {
-          console.log('run')
-            // get email input
-            // const event_type = this.getNodeParameter('event_type', 0) as string;
-            // const user_id = this.getNodeParameter('user_id', 0) as string;
+            const event_type = this.getNodeParameter('event_type', 0) as string;
+            const user_id = this.getNodeParameter('user_id', 0) as string;
+            const event_properties = this.getNodeParameter('event_properties', 0) as string;
+            const user_properties = this.getNodeParameter('user_properties', 0) as string;
 
-            // get additional fields input
-            const additionalFields = this.getNodeParameter('additionalFields', 0) as IDataObject;
+            const events: {}[] = [
+              {
+                'user_id': user_id,
+                'event_type': event_type,
+              }
+            ]
+
+            if (event_properties !== '') {
+              Object.assign(events[0], { 'event_properties': JSON.parse(event_properties) });
+            }
+
+            if (user_properties !== '') {
+              Object.assign(events[0], { 'user_properties': JSON.parse(user_properties) });
+            }
+
             const data: IDataObject = {
-                'api_key': 'e2bd4ddc941cdde04f5e2e8f91fbfce5',
-                'events': [
-                  {
-                    "user_id": 'ag@test.io',
-                    "event_type": 'event_type',
-                  }
-                ]
+              'api_key': credentials.apiKey,
+              'events': events
             };
-
-            Object.assign(data, additionalFields);
 
             const options: OptionsWithUri = {
                 headers: {
@@ -135,8 +236,6 @@ export class Amplitude implements INodeType {
             };
 
             responseData = await this.helpers.request(options);
-
-            console.log('responseData', responseData)
         }
     }
 
